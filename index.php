96618475cd3f089afb9d3f0b8995a2fdfcb2f6f2
alekzzz01 <96913449+alekzzz01@users.noreply.github.com>
2024-09-13 00:44:51 +0800

    first changes

--- index.php
+++ index.php
@@ -13,18 +13,21 @@ require './session/db.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Project</title>
 
-
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
     <script src="https://cdn.tailwindcss.com"></script>
 
-
+    <html data-theme="light"></html>
 
 </head>
 <body>
 
+    <div class="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+                <a class="btn btn-neutral" href="./session/login.php">Go to Login</a>
+    </div>
 
-        <a href="./session/login.php">Go to Login</a>
+   
     
 </body>
 </html>
